17c4bbe5740addeb34deb0b5d64f3cf59faef1a3
JhonnyLove1989 <jonathan.cosman1989@gmail.com>
2026-06-28 18:27:40 -0300

    Update update_ranking.py

--- update_ranking.py
+++ update_ranking.py
@@ -7,6 +7,9 @@ with open("games.json", "r", encoding="utf-8") as f:
 with open("palpites.json", "r", encoding="utf-8") as f:
     palpites = json.load(f)
 
+with open("palpites_mm.json", "r", encoding="utf-8") as f:
+    palpites_mm = json.load(f)
+
 ranking = []
 
 for pessoa, jogos in palpites.items():
@@ -47,7 +50,31 @@ for pessoa, jogos in palpites.items():
         "name": pessoa,
         "pts": pontos
     })
+# ✅ soma mata-mata também
+if pessoa in palpites_mm:
+    for p in palpites_mmjogo_real = next(
+            (g for g in games if g["team1"] == p["team1"] and g["team2"] == p["team2"] and g["date"] == p["date"]),
+            None
+        )
 
+        if not jogo_real:
+            continue
+
+        if jogo_real["score1"] is None or jogo_real["score2"] is None:
+            continue
+
+        s1 = jogo_real["score1"]
+        s2 = jogo_real["score2"]
+
+        if s1 == p["palpite1"] and s2 == p["palpite2"]:
+            pontos += 5
+        elif (
+            (s1 > s2 and p["palpite1"] > p["palpite2"]) or
+            (s2 > s1 and p["palpite2"] > p["palpite1"]) or
+            (s1 == s2 and p["palpite1"] == p["palpite2"])
+        ):
+            pontos += 3
+            
 # ordena ranking
 ranking.sort(key=lambda x: x["pts"], reverse=True)
 
